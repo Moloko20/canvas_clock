5172739755a51b3c7a8e32322b141d6b0e8bdc41
Alexander_V <moki1444@gmail.com>
2021-08-30 17:55:36 +0300

    perf(clock): final commit

--- main.js
+++ main.js
@@ -18,16 +18,15 @@ function canvasSetSize() {
 
 function clock() {
     const date = new Date();
-    // let hour = date.getHours() * (Math.PI / 6);
-    // let minute = date.getMinutes() * (Math.PI / 30);
-    // const second = date.getSeconds() + date.getMilliseconds() / 1000;
-    // const secArc = (second / 60) * 360;
-    // let ms = date.getMilliseconds() * (Math.PI / 1000);
-    const second = (date.getSeconds() * Math.PI) / 30;
 
-    // if (hour > 12) {
-    //     hour -= 12;
-    // }
+    const second = date.getSeconds() + date.getMilliseconds() / 1000;
+    const secondArc = (second * Math.PI) / 30;
+
+    const minute = date.getMinutes() + second / 60;
+    const minuteArc = (minute * Math.PI) / 30;
+
+    const hour = (date.getHours() % 12) + minute / 60;
+    const hourArc = (hour * Math.PI) / 6;
 
     ctx.clearRect(
         -ctx.canvas.width / 2,
@@ -37,81 +36,47 @@ function clock() {
     );
 
     //hour_hand
-    // ctx.save();
-    // ctx.strokeStyle = 'black';
-    // ctx.lineWidth = 7;
-    // ctx.rotate(hour + minute / 12 + second / 720 + ms / 21600);
-    // ctx.beginPath();
-    // ctx.moveTo(0, 0);
-    // ctx.lineTo(0, -100);
-    // ctx.stroke();
-    // ctx.closePath();
-    // ctx.restore();
+    ctx.beginPath();
+    ctx.strokeStyle = 'black';
+    ctx.lineWidth = 7;
+    ctx.moveTo(0, 0);
+    ctx.rotate(hourArc);
+    ctx.lineTo(0, -100);
+    ctx.stroke();
+    ctx.rotate(-hourArc);
+    ctx.closePath();
 
-    // //minute_hand
-    // ctx.save();
-    // ctx.strokeStyle = 'blue';
-    // ctx.lineWidth = 5;
-    // ctx.rotate(minute + second / 60 + ms / 1800);
-    // ctx.beginPath();
-    // ctx.moveTo(0, 0);
-    // ctx.lineTo(0, -200);
-    // ctx.stroke();
-    // ctx.closePath();
-    // ctx.restore();
+    //minute_hand
+    ctx.beginPath();
+    ctx.strokeStyle = 'blue';
+    ctx.lineWidth = 5;
+    ctx.moveTo(0, 0);
+    ctx.rotate(minuteArc);
+    ctx.lineTo(0, -200);
+    ctx.stroke();
+    ctx.rotate(-minuteArc);
+    ctx.closePath();
 
-    // //second_hand
-    // ctx.save();
+    //second_hand
 
     ctx.beginPath();
     ctx.strokeStyle = 'red';
     ctx.lineWidth = 2;
     ctx.moveTo(0, 0);
-    ctx.rotate(second);
+    ctx.rotate(secondArc);
     ctx.lineTo(0, -260);
     ctx.stroke();
+    ctx.rotate(-secondArc);
     ctx.closePath();
-    // ctx.restore();
 
-    // //central_circle
-    // ctx.save();
-    // ctx.fillStyle = 'red';
-    // ctx.lineWidth = 5;
-    // ctx.beginPath();
-    // ctx.arc(0, 0, 10, 0, 2 * Math.PI);
-    // ctx.fill();
-    // ctx.stroke();
-    // ctx.closePath();
-    // ctx.restore();
-
-    // //dial
-    // ctx.save();
-    // for (let i = 0; i < 12; i++) {
-    //     ctx.strokeStyle = 'black';
-    //     ctx.lineWidth = 2;
-    //     ctx.rotate(Math.PI / 6);
-    //     ctx.beginPath();
-    //     ctx.moveTo(0, 250);
-    //     ctx.lineTo(0, 260);
-    //     ctx.stroke();
-    //     ctx.closePath();
-    // }
-    // ctx.restore();
-
-    // ctx.save();
-    // for (let i = 1; i <= 12; i++) {
-    //     ctx.beginPath();
-    //     ctx.font = '36px serif';
-    //     let xText = 300 * Math.cos(30 * i * (Math.PI / 180) + Math.PI / -2);
-    //     let yText = 300 * Math.sin(30 * i * (Math.PI / 180) + Math.PI / -2);
-    //     if (i <= 9) {
-    //         ctx.fillText(i, xText - 10, yText + 14);
-    //     } else {
-    //         ctx.fillText(i, xText - 17, yText + 20);
-    //     }
-    //     ctx.closePath();
-    // }
-    // ctx.restore();
+    //central_circle
+    ctx.beginPath();
+    ctx.fillStyle = 'red';
+    ctx.lineWidth = 5;
+    ctx.arc(0, 0, 10, 0, 2 * Math.PI);
+    ctx.fill();
+    ctx.stroke();
+    ctx.closePath();
 
     window.requestAnimationFrame(clock);
 }
